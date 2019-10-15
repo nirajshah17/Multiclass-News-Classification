9df479ace4e93313975a1e7a44e668eec05d745d
nirajshah17 <55737082+nirajshah17@users.noreply.github.com>
2019-10-15 22:03:45 +0100

    Update thesis.R
    loop for data chunks

--- thesis.R
+++ thesis.R
@@ -1,15 +1,34 @@
+
 setwd("C:\\Users\\niraj\\OneDrive\\Desktop\\THESIS\\DataFinal")
-library(openxlsx)
+options(scipen = 999)
 
 file.info('news.csv')$size / 2^30
 readLines('news.csv',n=28)
 
 newsfile <- 'news.csv'
-chunksize <- 10000
+index <- 0
+chunksize <- 100000
 con <- file(description = newsfile , open = "r")
-data <- read.table(con, nrows = chunksize, header =  T , fill = T , sep = ",",quote = '"')
+datachunk <- read.table(con, nrows = chunksize, header =  T , fill = T , sep = ",",quote = '"')
+
+
+actualcolnames <- names(datachunk)
+
+repeat {
+  index <- index + 1
+  print(paste('Processing rows:', index * chunksize))
+  
+  
+  if (nrow(datachunk) != chunksize){
+    print('Processed all files!')
+    break}
+  
+  datachunk <- read.table(con, nrows=chunksize, skip=0, header=FALSE, fill = TRUE, sep=",", quote = '"', col.names=actualcolnames)
+  
+  if (index > 10) break
+  
+}
 close(con)
-head(data)
 
 #replace blanks with NA
 data[data==""]<-NA
