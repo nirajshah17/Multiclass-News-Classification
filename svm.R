7e6051b39de2af9236abcddc299f20d5eecec3e6
nirajshah17 <55737082+nirajshah17@users.noreply.github.com>
2019-11-07 16:41:53 +0000

    Update svm.R
    evaluation metrics - average preicsion , recall

--- svm.R
+++ svm.R
@@ -110,3 +110,25 @@ cMat
 #P-Value [Acc > NIR] : < 2.2e-16       
 
 #Kappa : 0.6098  
+cm <- as.matrix(table(svm_predict, valid_t$`dt1$type`))
+cm
+n <- sum(cm) # number of instances
+nc <- nrow(cm) # number of classes
+diag <- diag(cm) # number of correctly classified instances per class 
+rowsums <- apply(cm, 1, sum) # number of instances per class
+colsums <- apply(cm, 2, sum) # number of predictions per class
+p <-  rowsums / n # distribution of instances over the actual classes
+q <- colsums / n # distribution of instances over the predicted classes
+
+#accuracy
+accuracy <- sum(diag) / n 
+
+precision = diag / colsums 
+recall = diag / rowsums 
+f1 = 2 * precision * recall / (precision + recall) 
+
+data.frame(precision, recall, f1) 
+
+avg_Precision = mean(precision)
+avg_Recall = mean(recall)
+avg_F1 = mean(f1)
