3d77b2e55129f8da799bfb90972c9974a2601222
nirajshah17 <55737082+nirajshah17@users.noreply.github.com>
2019-11-18 14:08:40 +0000

    naive bayes for 100000 rows
    accuracy is 42.66%

--- naivebayes.R
+++ naivebayes.R
@@ -3,7 +3,6 @@ library(e1071)
 library(dplyr)
 library(caret)
 
-setwd("E:/data")
 options(scipen = 999)
 d1 <- read.csv("1cleaned.csv", stringsAsFactors = F)
 d2 <- d1
@@ -24,17 +23,17 @@ prop.table(count_type)
 barplot(count_type)
 
 d3 <- d2
-d2 <- d2[,-5]
+#d2 <- d2[,-5]
 
 #remove special characters
 d3$content <- gsub("[[:punct:]]", "", d3$content)
 d3$content <- gsub("â€™", "'", d3$content)
 
-dt1 <- d3[1:50000,]
+dt1 <- d3[1:100000,]
 
 
 #vectorsource considers each element in the vector as a document
-vs <- VectorSource(dt1$content) 
+vs <- VectorSource(dt1$content)
 
 # build corpus
 corpus <- Corpus(vs)  
@@ -49,7 +48,7 @@ corpus <- tm_map(corpus, removeNumbers)
 inspect(corpus[1:3])
 
 # remove puntucations
-corpus <- tm_map(corpus, removePunctuation) 
+corpus <- tm_map(corpus, removePunctuation)
 
 # remove unnecessary white spaces
 corpus <- tm_map(corpus, stripWhitespace)
@@ -60,22 +59,22 @@ corpus <- tm_map(corpus, removeWords, stopwords('english'))
 #covert to lower
 corpus <- tm_map(corpus, tolower)
 # build document term matrix
-tdm <- DocumentTermMatrix(corpus) 
+tdm <- DocumentTermMatrix(corpus)
 
 # remove sparse terms
-tdm_sparse <- removeSparseTerms(tdm, 0.90) 
+tdm_sparse <- removeSparseTerms(tdm, 0.90)
 
 # count matrix
-tdm_dm <- as.data.frame(as.matrix(tdm_sparse)) 
+tdm_dm <- as.data.frame(as.matrix(tdm_sparse))
 
 # binary instance matrix
-tdm_df <- as.matrix((tdm_dm > 0) + 0) 
+#tdm_df <- as.matrix((tdm_dm > 0) + 0)
 
-tdm_df <- ifelse(tdm_df[] > 0, "Yes", "No")
+tdm_df <- ifelse(tdm_dm[] > 0, "Yes", "No")
 #tdm_df[1:50000,] <- as.factor(tdm_df[1:50000,])
 
 tdm_df <- as.data.frame(tdm_df)
- 
+
 str(tdm_df)
 # append type class from original dataset
 tdm_df <- cbind(tdm_df, dt1$type)
@@ -84,21 +83,16 @@ set.seed(1703)
 
 #create training and testing datasets
 index <- sample(1:nrow(tdm_df), nrow(tdm_df) * .80, replace=FALSE)
-training <- tdm_df[index, ] 
+training <- tdm_df[index, ]
 testing <- tdm_df[-index, ]
 
-#create training and validation datasets from training 
+#create training and validation datasets from training
 index1 <- sample(1:nrow(training), nrow(training) * .80, replace=FALSE)
-training_t <- tdm_df[index1, ] 
+training_t <- tdm_df[index1, ]
 valid_t <- tdm_df[-index1, ]
 
 # Train the classifier
 system.time( classifier <- naiveBayes(training_t, training_t$`dt1$type`, laplace = 1) )
-system.time( pred <- predict(classifier, newdata = valid_t[, -302]) )
-(nbAccuracy <- 1- mean(pred != valid_t$`dt1$type`))
+system.time( pred <- predict(classifier, newdata = valid_t[, -274]) )
+(nbAccuracy <- 1- mean(pred != valid_t$`dt1$type`)) #accuracy = 42.66%
 cMat <- confusionMatrix(pred, valid_t$`dt1$type`) 
-cMat
-#Accuracy : 0.5435               
-#95% CI : (0.5362, 0.5508)     
-#No Information Rate : 0.4661               
-#P-Value [Acc > NIR] : < 0.00000000000000022
